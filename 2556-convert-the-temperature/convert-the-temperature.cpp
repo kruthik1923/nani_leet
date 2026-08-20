1839817a7b4582e7010345df5ee68089b7ef5ed9
Kruthik Polampalli <kruthikpolampalli@gmail.com>
2026-08-21 02:20:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (96.78%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -3,7 +3,7 @@ public:
     vector<double> convertTemperature(double celsius) {
         vector<double> arr;
         double k = celsius + 273.15;
-        double f = celsius*1.8 + 32.00;
+        double f = (celsius*1.8) + 32.00;
         arr.push_back(k);
         arr.push_back(f);
         return arr;
