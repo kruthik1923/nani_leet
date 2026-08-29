e2cf76a0105bd368c073a606b3e0c8c7f18b73f4
Kruthik Polampalli <kruthikpolampalli@gmail.com>
2026-08-30 00:42:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (55.08%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        vector<int> v;
+        int sum=0;
+        for(int i=0;i<accounts.size();i++){
+            sum=0;
+            for(int j=0;j<accounts[i].size();j++){
+                sum+=accounts[i][j];
+            }
+            v.push_back(sum);
+        }
+        return *max_element(v.begin(),v.end());
+    }
+};
