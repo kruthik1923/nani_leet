fce6e0e488d27a47f0ba30fd45ffd69055c2e0a5
Kruthik Polampalli <kruthikpolampalli@gmail.com>
2026-08-30 21:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (96.62%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-        bool check(int x,int n){
-            int ls=0,rs=0;
-            for(int i=1;i<=x;i++){
-                ls+=i;
-            }
-            for(int i=x;i<=n;i++){
-                rs+=i;
-            }
-            if(ls==rs){
-                return 1;
-            }
-            return 0;
-        }
     int pivotInteger(int n) {
+        int total = n * (n + 1) / 2;
+        int leftSum = 0;
 
-        int ans=0;
-        bool p=0;
-        for(int i=1;i<=n;i++){
-            p=check(i,n);
-            if(p==1){
-                return i;
+        for (int x = 1; x <= n; x++) {
+            leftSum += x;
+
+            int rightSum = total - leftSum + x;
+
+            if (leftSum == rightSum) {
+                return x;
             }
         }
+
         return -1;
     }
 };
