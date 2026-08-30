b72ce26969874819ceab91044020d827e18effef
Kruthik Polampalli <kruthikpolampalli@gmail.com>
2026-08-30 21:26:38 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (57.11%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector<int> v;
+        int flag=0;
+        for(int i=0;i<prices.size();i++){
+            flag=0;
+            for(int j=i+1;j<prices.size();j++){
+                if(prices[j]<=prices[i]){
+                    v.push_back(prices[i]-prices[j]);
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                v.push_back(prices[i]);
+            }
+        }
+        return v;
+    }
+};
