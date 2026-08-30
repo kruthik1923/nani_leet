8dd834e810934ec7a7eb12b9f2e72c41f87f8556
Kruthik Polampalli <kruthikpolampalli@gmail.com>
2026-08-30 22:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (80.44%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> v1,v2;
+            v1.push_back(nums[0]);
+            v2.push_back(nums[1]);
+        for(int i=2;i<nums.size();i++){
+            if(v1.back()>v2.back()){
+                 v1.push_back(nums[i]);
+            }
+            else{
+                v2.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<v2.size();i++){
+            v1.push_back(v2[i]);
+        }
+        return v1;
+    }
+};
