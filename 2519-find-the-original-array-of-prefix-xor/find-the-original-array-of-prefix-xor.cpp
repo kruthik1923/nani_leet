008d7038fdcfb65db14ee0b555f6470053e1a308
Kruthik Polampalli <kruthikpolampalli@gmail.com>
2026-09-06 17:29:10 +0530

    Time: 4 ms (23.90%) | Memory: 80.6 MB (43.19%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
-        vector<int> v;
-        v.push_back(pref[0]);
-        for(int i=1;i<pref.size();i++){
-            v.push_back(pref[i-1]^pref[i]);
-        }
-        return v;
+    for(int i=pref.size()-1;i>0;i--){
+        pref[i] = pref[i]^pref[i-1];
+    }   
+     return pref;
     }
 };
