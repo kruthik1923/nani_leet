6e820fb39ef4ed8b2a97f47168dbf18b77501ef7
Kruthik Polampalli <kruthikpolampalli@gmail.com>
2026-09-07 20:46:14 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (11.11%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int s=0,d=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<10){s+=nums[i];}
+            else{d+=nums[i];}
+        }
+        return (s!=d);
+    }
+};
