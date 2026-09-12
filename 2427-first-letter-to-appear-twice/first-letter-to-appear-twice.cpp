9e14b1b23d858c63b890f61ce58657d09cfc29bc
Kruthik Polampalli <kruthikpolampalli@gmail.com>
2026-09-12 22:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (69.77%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        vector<int> v(26,0);
+        for(int i=0;i<s.length();i++){
+            v[s[i]-'a']++;
+            if(v[s[i]-'a']==2){
+                return s[i];
+            }
+        }
+        return 'a';
+    }
+};
